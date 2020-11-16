a75a1a9ae21d9ae3e47f6f7c8df0ba8a61b37d65
Rubén Díaz Martínez <rubendm92@gmail.com>
2020-11-16 17:47:35 +0100

    Proper types for Event

--- packages/reffects/index.d.ts
+++ packages/reffects/index.d.ts
@@ -1,6 +1,12 @@
+type EventId = string;
+
 interface Event {
-    id: string;
-    payload?: object;
+    id: EventId;
+    payload?: any;
+}
+
+interface DelayedEvent extends Event {
+    milliseconds: number;
 }
 
 interface Effect {
@@ -19,13 +25,16 @@ type EffectHandler = Function;
 type CoeffectHandler = Function;
 
 export function dispatch(event: Event): void;
-export function dispatchMany(events: Event[]): void;
-export function dispatchLater(event: Event): void;
-export function registerEventHandler(eventId: string, handler: EventHandler, coeffectDescriptions?: Coeffect[]): void;
+export function dispatch(eventId: EventId): void;
+export function dispatchMany(events: Event|EventId[]): void;
+export function dispatchLater(event: DelayedEvent): void;
+
+export function registerEventHandler(eventId: EventId, handler: EventHandler, coeffectDescriptions?: Coeffect[]): void;
 export function registerCoeffectHandler(coeffectId: string, handler: CoeffectHandler): void;
 export function registerEffectHandler(effectId: string, handler: EffectHandler): void;
-export function registerEventsDelegation(originalEvents: string[], targetEvent: string): void;
+export function registerEventsDelegation(originalEvents: EventId[], targetEvent: EventId): void;
+
 export function coeffect(id: string, data?: object): Coeffect;
 export function getEffectHandler(effectId: string): EffectHandler; 
 export function getCoeffectHandler(coeffectId: string): CoeffectHandler;
-export function getEventHandler(eventId: string): EventHandler;
+export function getEventHandler(eventId: EventId): EventHandler;
